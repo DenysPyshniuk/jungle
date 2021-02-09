499f7e0703639df1bf771ca27467499bf1f96612
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-02-08 21:24:33 -0800

    Add logic to create a user before each test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    # CREATE NEW USER BEFORE EACH TEST
+    before (:each) do
+      @user = User.new(name:"Denys Pyshniuk", email: "denys@gmail.com", password: "password", password_confirmation: "password")
+      @user.save!
+    end
+
+  end
 end
