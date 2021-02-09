36c6f5b4ec18c61ff705904ddce6f7848db8312e
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-02-08 19:17:20 -0800

    Add validation on save a new product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+  # CREATE NEW CATEGORY AND PRODUCT BEFORE EACH TEST
+    before do
+      @category = Category.new(name: "Games")
+      @category.save
+      @product = Product.new(:name => "Willainous", :price => 54, :quantity => 5, :category => @category )
+      @product.save
+    end
+
+    it "ensures that product with all fields will save successfully" do
+        expect(@product).to be_valid
+    end
+
+
+
+
+
+
+  end
 end
